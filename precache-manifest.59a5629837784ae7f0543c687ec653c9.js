df7b9c3d34bef0c53cd9a509627f880e0d00a760
pson0001 <pson0001@hotmail.com>
2019-03-30 20:20:05 +1100

    Updates

--- precache-manifest.59a5629837784ae7f0543c687ec653c9.js
+++ precache-manifest.59a5629837784ae7f0543c687ec653c9.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/pingss-cv/static/media/signin.d1402f85.jpg"
   },
   {
-    "revision": "2825f3411b275ed1b8ea",
-    "url": "/pingss-cv/static/css/main.6f20011f.chunk.css"
+    "revision": "d20d70c5b6b5a4aa7652",
+    "url": "/pingss-cv/static/css/main.fde00bb4.chunk.css"
   },
   {
     "revision": "c19b3127aa5426ad47c4",
@@ -52,16 +52,16 @@ self.__precacheManifest = [
     "url": "/pingss-cv/static/media/beer.f7adcbef.jpg"
   },
   {
-    "revision": "08bac64cdc7329a50f4415bfaa9ec990",
-    "url": "/pingss-cv/static/media/contact.08bac64c.svg"
+    "revision": "ba7c6346cb863f6855454a95dcd63e30",
+    "url": "/pingss-cv/static/media/work.ba7c6346.svg"
   },
   {
     "revision": "7cf2139131cf4e1288358d492c2b211a",
     "url": "/pingss-cv/static/media/info.7cf21391.svg"
   },
   {
-    "revision": "ba7c6346cb863f6855454a95dcd63e30",
-    "url": "/pingss-cv/static/media/work.ba7c6346.svg"
+    "revision": "08bac64cdc7329a50f4415bfaa9ec990",
+    "url": "/pingss-cv/static/media/contact.08bac64c.svg"
   },
   {
     "revision": "287a3d457cef2514055d0d1a2dc03590",
@@ -104,8 +104,8 @@ self.__precacheManifest = [
     "url": "/pingss-cv/static/media/signup.f84402b5.jpg"
   },
   {
-    "revision": "2825f3411b275ed1b8ea",
-    "url": "/pingss-cv/static/js/main.2825f341.chunk.js"
+    "revision": "d20d70c5b6b5a4aa7652",
+    "url": "/pingss-cv/static/js/main.d20d70c5.chunk.js"
   },
   {
     "revision": "f637cc77533dc1dcd4fbf8e218ff8ede",
@@ -208,7 +208,7 @@ self.__precacheManifest = [
     "url": "/pingss-cv/static/media/final.dd2cb557.jpg"
   },
   {
-    "revision": "ba93f8c17d44ce3f5d39499778eac4c6",
+    "revision": "782b6b9d40bac5b05fd3b1f3d1a8641a",
     "url": "/pingss-cv/index.html"
   }
 ];
